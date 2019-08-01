b82600bc1e5a2c8cab70e92e9c43fb0e3f0ada66
Dao Han <e0014798@u.nus.edu>
2019-08-01 09:36:05 +0800

    basic inline keyboard

--- botActions/botAboutMe.js
+++ botActions/botAboutMe.js
@@ -1,4 +1,13 @@
 const fbFunc = require('../firebaseFunctions.js');
+const Telegraf = require('telegraf')
+const Extra = require('telegraf/extra')
+const Markup = require('telegraf/markup')
+const markup = Extra.markup(
+  Markup.inlineKeyboard([
+    Markup.gameButton('🎮 Play now!'),
+    Markup.urlButton('Telegraf help', 'http://telegraf.js.org')
+  ])
+)
 const {
   notStartedError,
   ERROR_MESSAGE,
@@ -15,7 +24,14 @@ module.exports = (bot, db) => {
         const { chatID, name } = data;
         console.log(data);
         if (typeof chatID === 'number') {
-          return ctx.reply(aboutMeMessage);
+          console.log('nabu');
+          return ctx.reply('Special buttons keyboard', Extra.markup((markup) => {
+            return markup.resize()
+              .keyboard([
+                markup.contactRequestButton('Send contact'),
+                markup.locationRequestButton('Send location')
+              ])
+          }));
         } else {
           return ctx.reply(notStartedError(name));
         }
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const config = require('./config/config.json');
 
-<<<<<<< HEAD
 const { db, bot } = require('./init');
 let {
   getDifferences,
@@ -28,12 +27,3 @@ botStop(bot, db);
 bot.on('sticker', ctx => ctx.reply('👍'));
 bot.hears('hi', ctx => ctx.reply('Hey there'));
 bot.launch();
-=======
-const bot = new Telegraf(config.apikey)
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.command('asdf', (ctx) => ctx.reply('yoyoyo'))
-bot.launch()
->>>>>>> 412adb084eff3d4d9a6efc4bf6ece9411785ee48
